e517cd2e42429fb087e91cc49b834e4138c97235
Dan Moore <dan@makeitdoathing.com>
2016-04-28 13:07:48 -0400

    fixing timeline?

--- sharedCode/path/Path.h
+++ sharedCode/path/Path.h
@@ -5,9 +5,16 @@
 
 class Path{
 public:
+    Path(){};
+    ~Path(){};
     virtual void setup(){};
+    virtual ofMatrix4x4 getNextPose(){ return ofMatrix4x4();};
     virtual void addPoint(ofVec3f pt){};
     virtual void addPath(vector<ofVec3f> pts){};
     virtual void addPath(ofPolyline line){};
     virtual void addPaths(vector<ofPolyline> lines){};
+    virtual void draw(){};
+    int ptIndex;
+    virtual int size(){return 0;};
+    ofPolyline path;
 };
